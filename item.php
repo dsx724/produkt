7b225e9c14f63a7d6968cc31b1adaddd29f507b7
Da Xue <dx@ubuntu.(none)>
2012-08-15 16:33:55 -0400

    Changes
    Signed-off-by: Da Xue <dx@ubuntu.(none)>

---
+++ item.php
@@ -0,0 +1,18 @@
+<?php
+
+class Item {
+	private $id;
+	private $name;
+	private $code;
+	private $quantity;
+	private $msrp;
+	private $price;
+	private $cost;
+	private $mtime;
+	private $ctime;
+	public static function get($item_id){
+		
+	}
+}
+
+?>
---
+++ product.php
@@ -0,0 +1,15 @@
+<?php
+
+class Product {
+	private $id;
+	private $name;
+	private $description;
+	private $active;
+	private $mtime;
+	private $ctime;
+	public static function get($product_id){
+		
+	}
+}
+
+?>
